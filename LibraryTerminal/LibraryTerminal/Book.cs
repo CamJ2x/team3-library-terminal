e4d33cfb9adf22abc06abaee1c2fa87b458b54f1
Cameron Johnson <cjohnson22@msu.idserve.net>
2025-05-21 18:53:20 -0400

    loose code

--- LibraryTerminal/LibraryTerminal/Book.cs
+++ LibraryTerminal/LibraryTerminal/Book.cs
@@ -1,11 +1,10 @@
 namespace LibraryTerminal;
-
 public class Book
 {
     public string Title { get; set; }
     public string Author { get; set; }
     public bool IsCheckedOut { get; set; }
-    public int DueDate { get; set; }
+    public int DueDate { get; set; } 
 
     public Book(string title, string author, bool isCheckedOut, int dueDate)
     {
